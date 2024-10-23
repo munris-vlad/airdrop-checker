b0eab7d873c8765a53fa970d8766d2e34f4cfec3
SIARHEI PALISHCHUK <smeshny@gmail.com>
2024-10-23 11:09:26 +0530

    Initial csv fix for grass

--- checkers/grass.js
+++ checkers/grass.js
@@ -1,6 +1,7 @@
 import {
     getKeyByValue,
-    readWallets
+    readWallets,
+    ensureDirectoryExistence
 } from '../utils/common.js'
 import axios from "axios"
 import { Table } from 'console-table-printer'
@@ -131,8 +132,11 @@ async function fetchWallets() {
         sort: (row1, row2) => +row1.n - +row2.n
     })
 
+    const csvFilePath = './results/grass.csv'
+    ensureDirectoryExistence(csvFilePath)
+
     csvWriter = createObjectCsvWriter({
-        path: './results/grass.csv',
+        path: csvFilePath,
         header: headers
     })
 
